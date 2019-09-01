84207bf8873f8055480acaea4e0ca8c892506b18
Jonathan Dain Palacio <bangarangler@gmail.com>
2019-09-01 17:59:12 -0400

    Scoped Styling

--- src/App.svelte
+++ src/App.svelte
@@ -1,4 +1,6 @@
 <script>
+  import ContactCard from './ContactCard.svelte';
+
   let name = 'Jon';
   let age = 31;
 
@@ -33,4 +35,8 @@
 <h1>Hello {uppercaseName}, my age is {age}!</h1>
 <button on:click="{incrementAge}">Change Age</button>
 <!--<button on:click="{changeName}">Change Name</button>-->
-<input type="text" value={name} on:input={nameInput}>
+<!--<input type="text" value={name} on:input={nameInput}>-->
+<!--two way data binding below-->
+<input type="text" bind:value={name} />
+
+<ContactCard />
